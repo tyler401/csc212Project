86df08f9cf7aab79caf1eb6a2a03a2875fc58069
Cameron <cammcgurer@uri.edu>
2022-04-13 17:00:49 -0400

    created a main and completed insertion sort

---
+++ sorting_algorithms.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+void insertionSort();
+void mergeSort();
+void quickSort();
+void heapSort();
+
+int main(int argc, char** argv){
+    std::string sorting_type = argv[1];
+    std::string input_file = argv[2];
+    std::string output_file = argv[3];
+
+    std::vector<int> data;
+
+    if (sorting_type == "insertion"){
+        insertionSort(data);
+    }
+    if (sorting_type == "merge"){
+        mergeSort(data);
+    }
+    if (sorting_type == "quick"){
+        quickSort(data);
+    }
+    if (sorting_type == "heap"){
+        heapSort(data);
+    } else {
+        std::cout << "Did not select valid sorting algorithm.\n";
+    }
+}
+
+void insertionSort(std::vector<int> &data){
+    int temp;
+    for (unsigned int i = 0; i < data.length(); i++){
+        for (unsigned int j = i; j > 0; j--){
+            if (data[j] < data[j - 1]){
+                temp = data[j];
+                data[j] = data[j - 1];
+                data[j - 1] = temp;
+            } else {
+                break;
+            }
+        }
+    }
+}
