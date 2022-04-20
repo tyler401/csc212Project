2cf5f3eaef7f92fcc940d3c27b8bbe4b83a71074
EricsenSemedo <99306164+EricsenSemedo@users.noreply.github.com>
2022-04-19 22:30:32 -0400

    Quick Fix

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -12,11 +12,11 @@
 //
 
 void insertionSort(std::vector<int> &data);
-void mergeSort(std::vector<int> &data);
-int partition(std::vector<int> &data, int low, int high);
-void r_quickSort(std::vector<int> &data, int low, int high);
-void quickSort(std::vector<int> &data);
-void heapSort(std::vector<int> &data);
+// void mergeSort(std::vector<int> &data);
+// int partition(std::vector<int> &data, int low, int high);
+// void r_quickSort(std::vector<int> &data, int low, int high);
+// void quickSort(std::vector<int> &data);
+// void heapSort(std::vector<int> &data);
 
 void readFile(std::string &fname, std::vector<int> &data);
 void writeFile(std::string &fname, std::vector<int> &data);
@@ -109,78 +109,78 @@ void insertionSort(std::vector<int> &data){
     }
 }
 
-void merge(int *A, int *aux, int lo, int mid, int hi) {
-// copy array
-    std::memcpy(aux+lo, A+lo, (hi-lo+1) * sizeof(int));
-// merge
-    int i=lo,j= mid +1;
-    for (int k= lo ; k<= hi: k++){
-        if (i > mid) A[k]= aux[j++];
-        else if (j > hi) A[k] = aux[i++];
-        else if (aux[j] < aux[i]) A[k]= aux[j++];
-        else A[k]= aux[i++];
-        }
-}
+// void merge(int *A, int *aux, int lo, int mid, int hi) {
+// // copy array
+//     std::memcpy(aux+lo, A+lo, (hi-lo+1) * sizeof(int));
+// // merge
+//     int i=lo,j= mid +1;
+//     for (int k= lo ; k<= hi: k++){
+//         if (i > mid) A[k]= aux[j++];
+//         else if (j > hi) A[k] = aux[i++];
+//         else if (aux[j] < aux[i]) A[k]= aux[j++];
+//         else A[k]= aux[i++];
+//         }
+// }
 
-void r_mergesort(int *A, int *aux, int lo, int hi){
-// base case (single element or empty list)
-     if (hi<= lo) return;
-    int mid = lo + (hi-lo) / 2;
-    // recursively sort halves
-    r_mergesort(A, aux, lo, mid);
-    r_mergesort (A, aux, mid+1, hi);
-// merge results
-    merge(A, aux, lo, mid, hi);
-}
+// void r_mergesort(int *A, int *aux, int lo, int hi){
+// // base case (single element or empty list)
+//      if (hi<= lo) return;
+//     int mid = lo + (hi-lo) / 2;
+//     // recursively sort halves
+//     r_mergesort(A, aux, lo, mid);
+//     r_mergesort (A, aux, mid+1, hi);
+// // merge results
+//     merge(A, aux, lo, mid, hi);
+// }
 
-void mergesort (int *A, int n){
-    int *aux = new int[n];
-    r_mergesort (A, aux, 0, n-1) ;
-    delete [] aux;
-}
+// void mergesort (int *A, int n){
+//     int *aux = new int[n];
+//     r_mergesort (A, aux, 0, n-1) ;
+//     delete [] aux;
+// }
 
-int partition(std::vector<int> &data, int low, int high){
-    int i = low;
-    int j = high + 1;
-    while (1) {
-        // while data[i] is less than the pivot, increase i
-        while (data[++i] < data[low]){
-            if (i == high){
-                break;
-            }
-        }
-        // while data[i] is greater than the pivot, decrease j
-        while (data[low] < data[--j]){
-            if (j == low){
-                break;
-            }
-        }
-        // if i and j cross, break
-        if (i >= j){
-            break;
-        } 
-        std::swap(data[i], data[j]);
-    }
-    // swap the pivot with data[j]
-    std::swap(data[low], data[j]);
-    // return the pivot's location
-    return j;
-}
-void r_quickSort(std::vector<int> &data, int low, int high){
-    if (high <= low){
-        return;
-    }
-    int p = partition(data, low, high);
-    r_quickSort(data, low, p - 1);
-    r_quickSort(data, p + 1, high);
-}
+// int partition(std::vector<int> &data, int low, int high){
+//     int i = low;
+//     int j = high + 1;
+//     while (1) {
+//         // while data[i] is less than the pivot, increase i
+//         while (data[++i] < data[low]){
+//             if (i == high){
+//                 break;
+//             }
+//         }
+//         // while data[i] is greater than the pivot, decrease j
+//         while (data[low] < data[--j]){
+//             if (j == low){
+//                 break;
+//             }
+//         }
+//         // if i and j cross, break
+//         if (i >= j){
+//             break;
+//         } 
+//         std::swap(data[i], data[j]);
+//     }
+//     // swap the pivot with data[j]
+//     std::swap(data[low], data[j]);
+//     // return the pivot's location
+//     return j;
+// }
+// void r_quickSort(std::vector<int> &data, int low, int high){
+//     if (high <= low){
+//         return;
+//     }
+//     int p = partition(data, low, high);
+//     r_quickSort(data, low, p - 1);
+//     r_quickSort(data, p + 1, high);
+// }
 
-void quickSort(std::vector<int> &data){
-    int n = data.size();
-    std::random_shuffle(data.begin(), data.begin() + n);
-    r_quickSort(data, 0, n - 1);
-}
+// void quickSort(std::vector<int> &data){
+//     int n = data.size();
+//     std::random_shuffle(data.begin(), data.begin() + n);
+//     r_quickSort(data, 0, n - 1);
+// }
 
-void heapSort(std::vector<int> &data){
+// void heapSort(std::vector<int> &data){
 
-}
+// }
