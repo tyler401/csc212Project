cf6a7a5762fb9dcb231674aeba2eb47cf3ae3653
Tyler Makhoul <tylerem17@gmail.com>
2022-04-21 17:58:17 -0400

    got merge working

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -7,12 +7,13 @@
 #include <cstdint>
 #include <cstring>
 #include <sstream>
+#include <random>
 
 // -- Function declaration
 //
 
 void insertionSort(std::vector<int> &data);
-void mergeSort(std::vector<int> &data);
+void mergesort(std::vector<int> &A);
 int partition(std::vector<int> &data, int low, int high);
 void r_quickSort(std::vector<int> &data, int low, int high);
 void quickSort(std::vector<int> &data);
@@ -22,9 +23,15 @@ void readFile(std::string &fname, std::vector<int> &data);
 void writeFile(std::string &fname, std::vector<int> &data);
 
 // -- Main --
-//
 
 int main(int argc, char** argv){
+    if (argc != 4)
+    {
+        std::cout << std::endl;
+        std::cout << "Usage: ./sorting_algorithm <sort type> <input file> <output file>";
+        std::cout << std:: endl << std::endl;
+        exit(0);
+    }
     std::string sorting_type = argv[1];
     std::string input_file = argv[2];
     std::string output_file = argv[3];
@@ -37,16 +44,18 @@ int main(int argc, char** argv){
         writeFile(output_file, data);
     }
     else if (sorting_type == "merge"){
-        // mergeSort(data);
-        // writeFile(output_file, data);
+       
+        mergesort(data);
+        writeFile(output_file, data);
     }
     else if (sorting_type == "quick"){
        // quickSort(data);
        // writeFile(output_file, data);
     }
     else if (sorting_type == "heap"){
-       // heapSort(data);
-       // writeFile(output_file, data);
+        int n = sizeof(data) / sizeof(data[0]);
+        heapSort(data);
+        writeFile(output_file, data);
     }
     else {
         std::cout << "Did not select valid sorting algorithm.\n";
@@ -54,28 +63,28 @@ int main(int argc, char** argv){
 }
 
 // -- Functions --
-//
 
+// Read in text file
 void readFile(std::string &fname, std::vector<int> &data){
     std::string line;
     std::ifstream file(fname);
 
     if(file.is_open()){
-        while(getline(file,line)){
+        while(getline(file, line, ',')){
             std::istringstream iss(line);
             int token;
-
             while(iss >> token){
                 data.push_back(token);
             }
         }
-
         file.close();
-    }else{
+    }
+    else{
         std::cout << "Unable to open file" << std::endl;
     }
 }
 
+// Write out sorted file contents
 void writeFile(std::string &fname, std::vector<int> &data){
     std::ofstream outFile(fname);
     unsigned int lastNumIndex = data.size()-1;
@@ -96,6 +105,8 @@ void writeFile(std::string &fname, std::vector<int> &data){
     outFile.close();
 }
 
+// -- Sorting Algorithms --
+
 void insertionSort(std::vector<int> &data){
     for (unsigned int i = 0; i < data.size(); i++){
         for (unsigned int j = i; j > 0; j--){
@@ -113,8 +124,8 @@ void merge(int *A, int *aux, int lo, int mid, int hi) {
 // copy array
     std::memcpy(aux+lo, A+lo, (hi-lo+1) * sizeof(int));
 // merge
-    int i=lo,j= mid +1;
-    for (int k= lo ; k<= hi: k++){
+    int i=lo, j= mid +1;
+    for (int k= lo ; k<= hi; k++){
         if (i > mid) A[k]= aux[j++];
         else if (j > hi) A[k] = aux[i++];
         else if (aux[j] < aux[i]) A[k]= aux[j++];
@@ -133,9 +144,10 @@ void r_mergesort(int *A, int *aux, int lo, int hi){
     merge(A, aux, lo, mid, hi);
 }
 
-void mergesort (int *A, int n){
+void mergesort(std::vector<int> &A){
+    int n = A.size();
     int *aux = new int[n];
-    r_mergesort (A, aux, 0, n-1) ;
+    r_mergesort (&A[0], aux, 0, n-1) ;
     delete [] aux;
 }
 
@@ -177,10 +189,54 @@ void r_quickSort(std::vector<int> &data, int low, int high){
 
 void quickSort(std::vector<int> &data){
     int n = data.size();
-    std::random_shuffle(data.begin(), data.begin() + n);
+    // Create random device
+    std::random_device rand_dev;
+    // mt19937 = mersenne_twister_engine
+    std::mt19937 g(rand_dev());
+    // random_shuffle is no longer available changed to shuffle
+    std::shuffle(data.begin(), data.begin() + n, g);
     r_quickSort(data, 0, n - 1);
 }
 
-void heapSort(std::vector<int> &data){
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. n is size of heap
+void heapify(std::vector<int> &data, int n, int i)
+{
+	int largest = i; // Initialize largest as root
+	int l = 2 * i + 1; // left = 2*i + 1
+	int r = 2 * i + 2; // right = 2*i + 2
 
+	// If left child is larger than root
+	if (l < n && data[l] > data[largest])
+		largest = l;
+
+	// If right child is larger than largest so far
+	if (r < n && data[r] > data[largest])
+		largest = r;
+
+	// If largest is not root
+	if (largest != i) {
+		std::swap(data[i], data[largest]);
+
+		// Recursively heapify the affected sub-tree
+		heapify(data, n, largest);
+	}
+}
+
+// main function to do heap sort
+void heapSort(std::vector<int> &data)
+{
+    int n = data.size();
+	// Build heap (rearrange array)
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(data, n, i);
+
+	// One by one extract an element from heap
+	for (int i = n - 1; i >= 0; i--) {
+		// Move current root to end
+		std::swap(data[0], data[i]);
+
+		// call max heapify on the reduced heap
+		heapify(data, i, 0);
+	}
 }
